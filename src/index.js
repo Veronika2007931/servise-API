d7bd333bbf68dcffb7e23a25e9c6408514679ed5
Veronika2007931 <113263850+Veronika2007931@users.noreply.github.com>
2023-09-02 01:02:56 +0300

    add

--- src/index.js
+++ src/index.js
@@ -1,20 +1,19 @@
-// import pokemonTpl from './pokemon-cards.handlebars'
+import pokemonTpl from './pokemon-cards.handlebars'
 
-// const cardContainer = document.querySelector('.js-card-container')
+const cardContainer = document.querySelector('.js-card-container')
 
-// form.addEventListener('submit', (event)=>{
-// event.preventDefault()
-// const id = event.currentTarget.query.value
+form.addEventListener('submit', (event)=>{
+event.preventDefault()
+const id = event.currentTarget.query.value
 
-// fetch(`https://pokeapi.co/api/v2/pokemon/${value}`)
-// .then(response=>response.json())
-// .then((pokemon)=>{
-//     const markup = pokemonTpl(pokemon)
-//     console.log(markup)
+fetch(`https://pokeapi.co/api/v2/pokemon/15`).then(res => res.json())
+.then((pokemon)=>{
+    const markup = pokemonTpl(pokemon)
+    console.log(markup)
 
-//     cardContainer.innerHTML = markup
-// })
-// })
+    cardContainer.innerHTML = markup
+})
+})
 
 
 
