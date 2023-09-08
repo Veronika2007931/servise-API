28301487c2e1f32d88d7b3d4b07a80ffbc10917d
Veronika2007931 <113263850+Veronika2007931@users.noreply.github.com>
2023-09-08 18:03:51 +0300

    add hw

--- src/fetchCountries.js
+++ src/fetchCountries.js
@@ -1,7 +1,7 @@
 
 export function fetchCountries(searchQuery){
    return fetch(`https://restcountries.com/v3.1/name/${searchQuery}`)
-    .then(response=> response.json )
+    .then(response=> response.json() )
 
 }
 
--- src/hw-13.js
+++ src/hw-13.js
@@ -1,8 +1,8 @@
 
 import debounce from "lodash.debounce"
 import {fetchCountries} from "./fetchCountries"
-
-console.log(fetchCountries('Ukraine'))
+const box = document.querySelector(".js-box")
+const list = document.querySelector(".list")
 
 const input = document.querySelector(".input")
 input.addEventListener("input", debounce(onSearchQuery, 2000))
@@ -10,8 +10,34 @@ input.addEventListener("input", debounce(onSearchQuery, 2000))
 function onSearchQuery(event){
 
     fetchCountries(event.target.value)
-    .then((result)=>console.log(result))
-}
+    
+    .then((cantries)=>{
+        console.log(cantries)
+        if(cantries.length === 1){
+          const markap = `
+          <h1></h1>
+          <p>Capital: ${cantries.capital} </p>
+          <p>Population: ${cantries.population} </p>
+          <p>Lenguages: ${cantries.languages}</p>
+          <ul>
+              <li></li>
+              <li></li>
+              <li></li>
+          </ul>
+          <img src="" alt="" >
+          `
 
+          box.innerHTML = markap
+        }else if(cantries.length >= 2 && cantries.length <= 10){
+           const listCantries = cantries.map((cantrie)=>{
+               return ` <li>${cantrie.name.common}</li>`
+              
+           }).join(" ")
+              list.innerHTML = listCantries
+        }else{
+            alert("Too many results")
+        }
+    })
+}
 
 
